837ab3830fc6a9a40fb2e3d68b5de3366512ea5b
pseudobyte <pseudobyte@gmail.com>
2015-10-29 03:03:30 -0600

    initial commit

---
+++ rws/index.js
@@ -0,0 +1,134 @@
+var bkeys = ["b:Farm", "b:Inn", "b:Blacksmith", "b:DeepMine",
+"b:StonePillars", "b:AlchemistLab","b:Monastery", "b:Labyrinth",
+"b:IronStronghold", "b:AncientPyramid", "b:HallOfLegends"];
+
+var bnames = ["Farms", "Inns", "Blacksmiths", "Deep Mines",
+"Stone Pillars", "Alchemist Labs", "Monasteries", "Labyrinths",
+"Iron Strongholds", "Ancient Pyramids", "Halls of Legends"];
+
+document.addEventListener('paste', clipboardHandler);
+
+$(document).on("mousewheel DOMMouseScroll", function(e) {
+  var delta = Math.max(-1, Math.min(1, (e.originalEvent.wheelDelta || -e.originalEvent.detail)));
+  if (delta == -1 && ($(window).scrollTop() + $(window).height() == $(document).height())) {
+    addMoreHits();
+  }
+});
+
+function listJoin(a) {
+  if (a.length == 0) {
+    return "";
+  }
+  else if (a.length == 1) {
+    return a[0];
+  }
+  else if (a.length == 2) {
+    return a[0] + " and " + a[1];
+  }
+  else {
+    var list = a[0];
+    for (var i = 1; i < a.length-1;i++) {
+      list += ", " + a[i]
+    }
+    list += ", and " + a[a.length-1]
+    return list
+  }
+}
+
+function ulJoin(a) {
+  list = "<ul>"
+  for (i = 0; i < a.length; i++) {
+    list += "<li>" + a[i] + "</li>"
+  }
+  list += "</ul>"
+  return list
+}
+
+function clipboardHandler(e) {
+  var dat;
+  // IE
+  if (window.clipboardData && window.clipboardData.getData) {
+    dat = window.clipboardData.getData('text');
+  }
+  // chrome/firefox/safari
+  else {
+    dat = e.clipboardData.getData('text/plain');
+  }
+  processSave(dat);
+}
+
+function processSave(dat) {
+  document.getElementById("intro").innerHTML = "";
+  document.getElementById("buildings").innerHTML = "";
+  document.getElementById("forecast").innerHTML = "";
+  document.hits = undefined;
+  try {
+    dat = decode(dat);
+  }
+  catch(err) {
+    console.log(err);
+    document.getElementById("intro").innerHTML = "Your save is invalid.";
+    document.getElementById("forecast").innerHTML = "Forecast: no lightning.";
+    return
+  }
+  if (dat.alignment != 3) {
+    document.getElementById("intro").innerHTML = "You are not aligned neutral.";
+    document.getElementById("forecast").innerHTML = "Forecast: no lightning.";
+    return
+  }
+  var ownedBuildings = [];
+  for (var i = 0; i < bkeys.length; i++) {
+    if (dat.build[bkeys[i]].q > 0) {
+      ownedBuildings[ownedBuildings.length] = bnames[i];
+    }
+  }
+  if (!(dat.faction == 6 || dat.msp == "Lightning Strike"
+        || dat.msp2 == "Lightning Strike")) {
+    document.getElementById("intro").innerHTML = "You don't have lightning strike.";
+    document.getElementById("forecast").innerHTML = "Forecast: no lightning.";
+    return
+  }
+  if (ownedBuildings.length == 0) {
+    document.getElementById("intro").innerHTML = "You have no buildings.";
+    document.getElementById("forecast").innerHTML = "Forecast: no lightning.";
+    return
+  }
+  if (ownedBuildings.length == 1) {
+    document.getElementById("intro").innerHTML = "You only have " + ownedBuildings[0] + ".";
+    document.getElementById("forecast").innerHTML = "Forecast: the only thing you have, as many times as you like.";
+    return
+  }
+  var state = dat.spell["s:LightningStrike"].s;
+  document.getElementById("intro").innerHTML = "Your RNG state is: " + state + ".";
+  document.getElementById("buildings").innerHTML = "You have " + listJoin(ownedBuildings) + ".";
+  var RNG = new PM_PRNG(state);
+  var hits = [];
+  for (i = 0; i < 10; i++) {
+    hits.push(ownedBuildings[RNG.strikeTier(ownedBuildings.length)]);
+  }
+  hits[hits.length] = "<a onclick='addMoreHits()' href='#'>Give me a longer forecast...</a>"
+  document.getElementById("forecast").innerHTML = "Forecast: " + ulJoin(hits);
+  document.hits = hits;
+  document.RNG = RNG;
+  document.ownedBuildings = ownedBuildings;
+}
+
+function addMoreHits() {
+  if (!document.hits) {
+    return
+  }
+  var hits = document.hits;
+  var RNG = document.RNG;
+  var ownedBuildings = document.ownedBuildings;
+  hits.splice(hits.length-1, 1);
+  for (var i = 0; i < 10; i++) {
+    hits.push(ownedBuildings[RNG.strikeTier(ownedBuildings.length)]);
+  }
+  hits[hits.length] = "<a onclick='addMoreHits()' href='javascript:;'>Give me an even longer forecast...</a>"
+  document.getElementById("forecast").innerHTML = "Forecast: " + ulJoin(hits);
+  document.hits = hits;
+  document.RNG = RNG;
+  document.ownedBuildings = ownedBuildings;
+
+  return false;
+}
