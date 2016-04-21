df504032f85af5dd15eb0b8580c3c63d95377de5
dox4242 <dox4242@yahoo.com>
2016-04-20 23:12:46 -0700

    buildings are alignment sensitive

--- edit/index.js
+++ edit/index.js
@@ -8,15 +8,26 @@ var myViewModel = new Vue({
     showStatsReinc: 'False',
     showStatsAll: 'False',
     showBuildings: 'False',
-    showUpgrades: 'False'
+    showGoodBuildings: 'False',
+    showEvilBuildings: 'False',
+    showNeutralBuildings: 'False',
+    showHallsBuilding: 'False',
+    showUpgrades: 'False',
+    showTrophies: 'False',
+    showSpells: 'False',
+    showFCs: 'False',
+    showEventRes: 'False',
+    alignment: ''
   },
   watch: {
     datainput: function(data){
       this.version = ''
       try {
         this.save = SaveHandler.Decode(this.datainput)
-        console.log('Decoded save:', this.save)
+        save2 = SaveHandler.Decode(this.datainput)
+        console.log('Decoded save:', save2)
         this.editdata = 'Decoded. No edits made'
+        this.alignment = this.save.alignment
       } catch(err) {
           this.editdata = 'Invalid data'
           console.log(err)
@@ -48,8 +59,40 @@ var myViewModel = new Vue({
     toggleBuildings: function () {
       if (this.showBuildings == 'True') {
         this.showBuildings = 'False'
+        console.log('hide buildings')
+        if (this.save.alignment == 1) {
+          this.showGoodBuildings = 'False'
+          this.showHallsBuilding = 'False'
+          console.log('hide good buildings')
+        }
+        if (this.save.alignment == 2) {
+          this.showEvilBuildings = 'False'
+          this.showHallsBuilding = 'False'
+          console.log('hide evil buildings')
+       }
+        if (this.save.alignment == 3) {
+          this.showNeutralBuildings = 'False'
+          this.showHallsBuilding = 'False'
+          console.log('hide neutral buildings')
+        }
       } else {
         this.showBuildings = 'True'
+        console.log('show buildings')
+        if (this.save.alignment == 1) {
+          this.showGoodBuildings = 'True'
+          this.showHallsBuilding = 'True'
+          console.log('show good buildings')
+        }
+        if (this.save.alignment == 2) {
+          this.showEvilBuildings = 'True'
+          this.showHallsBuilding = 'True'
+          console.log('show evil buildings')
+        }
+        if (this.save.alignment == 3) {
+          this.showNeutralBuildings = 'True'
+          this.showHallsBuilding = 'True'
+          console.log('show neutral buildings')
+        }
       }
     },
     toggleUpgrades: function () {
@@ -57,6 +100,34 @@ var myViewModel = new Vue({
         this.showUpgrades = 'False'
       } else {
         this.showUpgrades = 'True'
+      }
+    },
+    toggleTrophies: function () {
+      if (this.showTrophies == 'True') {
+        this.showTrophies = 'False'
+      } else {
+        this.showTrophies = 'True'
+      }
+    },
+    toggleSpells: function () {
+      if (this.showSpells == 'True') {
+        this.showSpells = 'False'
+      } else {
+        this.showSpells = 'True'
+      }
+    },
+    toggleFCs: function () {
+      if (this.showFCs == 'True') {
+        this.showFCs = 'False'
+      } else {
+        this.showFCs = 'True'
+      }
+    },
+    toggleEventRes: function () {
+      if (this.showEventRes == 'True') {
+        this.showEventRes = 'False'
+      } else {
+        this.showEventRes = 'True'
       }
     }
   }
