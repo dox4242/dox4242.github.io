2d3d8c4f83997b1eb29916bcc340e6af05531050
dox4242 <dox4242@yahoo.com>
2016-04-27 23:04:31 -0700

    test-coding lightning streak

--- edit/index.js
+++ edit/index.js
@@ -5,6 +5,7 @@ var myViewModel = new Vue({
     oldsave: {},
     newsave: {},
     newdata: 'empty',
+
     alignmentinput: '',
     alignmentmsg: '',
     angellineinput: '',
@@ -14,6 +15,22 @@ var myViewModel = new Vue({
     bloodlineinput: '',
     bloodlineIDs: [194, 196, 39, 212, 396, 103, 380, 136, 183, 150, 120],
     bloodlinemsg: '',
+    lsinput: '',
+    lsmsg: '',
+    LightningRod: [[[9, 9, 8, 8, 10, 8, 10, 8, 8, 9, 9], [1348656067, 2066742637, 724761641, 1139776337, 1832831073, 2874410, 314652574, 29728603, 57855160, 80741010, 798827580]],
+                  [[9, 8, 10, 10, 9, 9, 10, 10, 8, 9], [1348656067, 195509834, 1092747531, 1369388690, 124895406, 1889967370, 778094957, 1054736116, 145769791, 798827580]],
+                  [[10, 9, 9, 10, 9, 10, 9, 9, 10], [1019766806, 1010581720, 293919951, 1832831073, 274387307, 314652574, 233911564, 571758769, 1127716841]],
+                  [[10, 10, 9, 10, 10, 9, 10, 10], [1019766806, 72980725, 44627026, 1267687915, 314652574, 142176518, 965174051, 1127716841]],
+                  [[10, 10, 10, 10, 10, 10, 10], [454623648, 54592755, 260059819, 404214639, 462244724, 255012428, 442091522]],
+                  [[12, 12, 12, 12, 12, 12], [770473881, 821234634, 250490584, 1896993063, 643171274, 89562065]],
+                  [[14, 13, 12, 13, 14], [770473881, 419517449, 527131743, 819876890, 1377009766]],
+                  [[15, 15, 15, 15], [1408849714, 355243782, 951868488, 738633933]],
+                  [[18, 17, 18], [918066031, 129106684, 1050527706]],
+                  [[28, 28], [1202476373, 447787816]]],
+    buildingcount: 0,
+    buildingnames: ['Alchemist Lab', 'Ancient Pyramid', 'Blacksmith', 'Cathedral', 'Citadel', 'Dark Temple', 'Deep Mine', 'Evil Fortress', 'Farm', 'Hall of Legends', 'Heaven\'s Gate', 'Hell Portal', 'Inn', 'Iron Stronghold', 'Knights Joust', 'Labyrinth', 'Monastery', 'Necropolis', 'Orcish Arena', 'Royal Castle', 'Slave Pen', 'Stone Pillars', 'Warrior Barracks', 'Witch Conclave', 'Wizard Tower'],
+    currentbuildings: [],
+    LSavail: 'False',
     reincinput: '',
     rubiesinput: '',
     scrymsg: ''
@@ -21,17 +38,25 @@ var myViewModel = new Vue({
   watch: {
     olddata: function(data) {
       try {
-        this.oldsave = SaveHandler.Decode(data)
+        this.alignmentmsg = ''    // erase possible prior output messages
+        this.angellineinput = ''
+        this.artRNGmsg = ''
+        this.ascensioninput = ''
+        this.bloodlinemsg = ''
+        this.lsmsg = ''
+        this.buildingcount = 0
+        this.currentbuildings = []
+        this.reincinput = ''
+        this.rubiesinput = ''
+        this.scrymsg = ''
+
+        this.oldsave = SaveHandler.Decode(data)  // decode stuff
         this.newsave = SaveHandler.Decode(data)
         console.log('Decoded save:', SaveHandler.Decode(data))
         this.newdata = 'Decoded v' + this.oldsave.saveVersion
-        for (var i = 0; i < this.oldsave.upgrades.length; i++) {
-          if (this.oldsave.upgrades[i].id == 39) {
-            if (this.oldsave.upgrades[i].u1 == true) {
-              this.angellineUpgradeBought = 'True'
-            }
-          }
-        }
+
+        this.checkAngellineUpgrade()     // check state and setup page fields
+        this.checkLSavail()
       } catch(err) {
           this.newdata = 'Invalid data'
           console.log(err)
@@ -80,14 +105,16 @@ var myViewModel = new Vue({
         this.newdata = SaveHandler.Encode(this.newsave)
         this.bloodlinemsg = 'Saved'
         return
-      }
-      else { 
+      } else { 
         for (var i = 0; i < this.oldsave.upgrades.length; i++) {
           for (var j = 0; j < this.bloodlineIDs.length; j++) {
             if (this.oldsave.upgrades[i].id == this.bloodlineIDs[j]) {
               if (this.bloodlineinput == 'Fairy') { this.newsave.upgrades[i].id = 194 }
               if (this.bloodlineinput == 'Elf') { this.newsave.upgrades[i].id = 164 }
-              if (this.bloodlineinput == 'Angel') { this.newsave.upgrades[i].id = 39 }
+              if (this.bloodlineinput == 'Angel') {
+                this.newsave.upgrades[i].id = 39
+                this.angellineUpgradeBought = 'True'
+              }
               if (this.bloodlineinput == 'Goblin') { this.newsave.upgrades[i].id = 212 }
               if (this.bloodlineinput == 'Undead') { this.newsave.upgrades[i].id = 396 }
               if (this.bloodlineinput == 'Demon') { this.newsave.upgrades[i].id = 103 }
@@ -103,9 +130,41 @@ var myViewModel = new Vue({
         this.bloodlinemsg = 'Saved'
         return
       }
+    },
+    lsinput: function(data) {
+      selIndex = this.currentbuildings.findIndex(x => x == this.lsinput)
+      rodIndex = 11 - this.buildingcount
+      console.log(this.LightningRod[rodIndex][1][selIndex])
     }
   },
   methods: {
+    checkAngellineUpgrade: function () {
+      this.angellineUpgradeBought = 'False'
+      for (var i = 0; i < this.oldsave.upgrades.length; i++) {
+        if (this.oldsave.upgrades[i].id == 39) {
+          if (this.oldsave.upgrades[i].u1 == true) {
+            this.angellineUpgradeBought = 'True'
+          }
+        }
+      }
+    },
+    checkLSavail: function () {
+      if (this.oldsave.faction != 6) {
+        if (this.oldsave.faction == 11) {
+          if (this.oldsave.mercSpell1 == 13 || this.oldsave.mercSpell2 == 13) {
+            this.LSavail = 'True'
+          }
+        }
+      } else { this.LSavail = 'True' }
+      this.buildingcount = 0
+      for (var i = 0; i < this.oldsave.buildings.length; i++) {
+        if (this.oldsave.buildings[i].q > 0) {
+          this.currentbuildings.push(this.buildingnames[this.oldsave.buildings[i].id-1])
+          this.buildingcount += 1
+        }
+      }
+      if (this.buildingcount < 2) { this.LSavail = 'False' }
+    },
     saveAngelline: function () {
       if (this.angellineinput == '') { return }
       try {
