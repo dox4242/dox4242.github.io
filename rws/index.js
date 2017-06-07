0e3cf9e3d48d54d4adf3f44772d390859a50f8b1
dox4242 <dox4242@yahoo.com>
2017-06-07 01:28:02 -0700

    adding titan perk 2 exception

--- rws/index.js
+++ rws/index.js
@@ -48,12 +48,14 @@
 			var lightningForecast = '';
 			
 			// Check if the save actually has Lightning Strikes to forecast
-			if (save.alignment != 3) {
+            console.log('test:',util.save.upgrade_owned(save, 688))
+			if (!(save.faction == 6 || save.mercSpell1 == 13 || save.mercSpell2 == 13 || util.save.upgrade_owned(save, 688))) {
+			 	lightningMessage = 'You don\'t have Lightning Strike.';
+			 	lightningForecast = 'No Lightning.';
+			} else if (save.alignment != 3 && !util.save.upgrade_owned(save, 688)) {
 				lightningMessage = 'You are not Neutral aligned.';
 				lightningForecast = 'No Lightning.';
-			// } else if (!(save.faction == 6 || save.mercSpell1 == 13 || save.mercSpell2 == 13)) {
-			// 	lightningMessage = 'You don\'t have Lightning Strike.';
-			// 	lightningForecast = 'No Lightning.';			} else if (buildingsOwned.length == 0) {
+            } else if (buildingsOwned.length == 0) {
 				lightningMessage = 'You have no Buildings.';
 				lightningForecast = 'No Lightning.';
 			} else if (buildingsOwned.length == 1) {
