de7f4458ea41e2e21c38f8c1c957194db643bc60
Curunur <40272706+Curunur@users.noreply.github.com>
2019-04-18 16:21:22 +0200

    Fixed Event Endtime

--- mri/index.js
+++ mri/index.js
@@ -94,7 +94,7 @@
       View.state = this.state = this.save.eggRngState;
 
       var now = new Date();
-      var end = new Date(Date.UTC(2019, 3, 25, 18, 59, 0));
+      var end = Date.UTC(2019, 3, 25, 18, 59, 0);
       var til_end = Math.floor((end - now) / 60000);
       var til_end_scry = Math.floor((end - now) / 60000 * 1.2);
       var next_stack = this.save.eggStackSize;
